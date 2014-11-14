f6ab3d1dbf8a57c443021ee0e620a936800f8ee6
Andrew Wagner <drewm1980@gmail.com>
2014-11-14 21:07:36 +0100

    Figured out how to do modules

--- omr/src/main.rs
+++ omr/src/main.rs
@@ -1,54 +1,33 @@
-#![crate_name = "music_ocr"]
+#![crate_name = "rust-omr"]
 #![crate_type = "rlib"]
-
-extern crate png;
-//use png::{Image,RGB8,store_png,load_rgba8,};
-use png::{load_png,Image};
 
 //use std::mem;
 //use std::io;
 //use std::io::File;
 //use std::ptr;
 //use std::slice;
+use std::os;
 
-#[cfg(test)]
-mod test {
-	extern crate test;
-	use super::png::{load_png,Image};
+use std::io::fs::PathExtensions;
 
-#[test]
-	fn test_easyload() {
-		let loadfile = "sheet_music/La_yumba1.png";
-		let loadpath = &Path::new(loadfile);
-		let img = match load_png(loadpath){
-			Ok(img) => img,
-			Err(e) => panic!("Could not ooopen file: {}",loadfile)
-};
+mod loaders;
+//use loaders::load;
 
-		println!("Image width is {}",img.width);
-		
-		//let storefile = "La_yumba1_modified.png";
-	}
-
-
-//#[test]
-	//fn test_store() {
-		//let mut img = Image {
-//width: 10,
-	       //height: 10,
-	       //pixels: RGB8(Vec::from_elem(10 * 10 * 3, 100u8)),
-		//};
-		//let res = store_png(&mut img, &Path::new("test/store.png"));
-		//assert!(res.is_ok());
-	//}
-//#[test]
-	//fn test_load() 
-	//{
-		//load_rgba8("test/servo-screenshot.png", 831, 624);
-		//load_rgba8("test/store.png", 10, 10);
-	//}
-}
-
+#[allow(dead_code)]
 fn main() {
-    println!("Hello, world!")
+	let args = os::args();
+	println!("Entering main...");
+	let path_to_load = match args.len() {
+		0|1 => {println!("Too few arguments passed!");
+			panic!("Usage: rust-omr PATH");},
+		2 => {println!("The first argument is {}", args[1]);
+			&args[1]},
+		_ => {println!("Too many arguments passed!");
+			panic!("Usage: rust-omr PATH");},
+	};
+		
+	let loadpath = Path::new(path_to_load);
+	if ! (loadpath.exists()) {panic!("The file {} does not exist!",path_to_load)}
+
+	loaders::load(&loadpath);
 }
