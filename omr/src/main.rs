bbcc0cba24a7f36a5ee32a2802d17f0e5f15e8bd
Andrew Wagner <drewm1980@gmail.com>
2014-11-15 11:12:20 +0100

    Formatting cleanup with properly configured vim

--- omr/src/main.rs
+++ omr/src/main.rs
@@ -15,19 +15,19 @@ mod loaders;
 
 #[allow(dead_code)]
 fn main() {
-	let args = os::args();
-	println!("Entering main...");
-	let path_to_load = match args.len() {
-		0|1 => {println!("Too few arguments passed!");
-			panic!("Usage: rust-omr PATH");},
-		2 => {println!("The first argument is {}", args[1]);
-			&args[1]},
-		_ => {println!("Too many arguments passed!");
-			panic!("Usage: rust-omr PATH");},
-	};
-		
-	let loadpath = Path::new(path_to_load);
-	if ! (loadpath.exists()) {panic!("The file {} does not exist!",path_to_load)}
+    let args = os::args();
+    println!("Entering main...");
+    let path_to_load = match args.len() {
+        0|1 => {println!("Too few arguments passed!");
+            panic!("Usage: rust-omr PATH");},
+            2 => {println!("The first argument is {}", args[1]);
+                &args[1]},
+                _ => {println!("Too many arguments passed!");
+                    panic!("Usage: rust-omr PATH");},
+    };
 
-	loaders::load(&loadpath);
+    let loadpath = Path::new(path_to_load);
+    if ! (loadpath.exists()) {panic!("The file {} does not exist!",path_to_load)}
+
+    loaders::load(&loadpath);
 }
