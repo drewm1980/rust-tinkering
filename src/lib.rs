2aa875ab49f21878d334370757379ca6582b0866
Andrew Wagner <drewm1980@gmail.com>
2014-12-18 21:45:24 +0100

    Creating a library to export symbols

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+// Rexport stuff so I can use it in other projects
+pub use slices_iterators::splice;
+pub use slices_iterators::split_while;
+pub use slices_iterators::take_while2 as take_while;
+
+mod slices_iterators;
